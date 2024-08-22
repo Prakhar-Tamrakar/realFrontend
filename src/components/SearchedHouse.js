4e2588d3aa575c9dd08b61770b1d40f8e20d89a0
Prakhar-Tamrakar <prakhar3091@gmail.com>
2024-08-22 12:54:37 +0530

    first commit

---
+++ src/components/SearchedHouse.js
@@ -0,0 +1,46 @@
+import { useLocation } from "react-router-dom";
+import House from "./House";
+import axios from "axios";
+import { useEffect,useState } from "react";
+
+
+
+    const SearchedHouse = (props) => {
+    let [searchedHouse,setSearchedHouse] =  useState({})
+        
+
+
+    let loca = useLocation();
+    console.log(loca.state.id);
+    // let searchedHouse = props.housesData.find( (elem) => elem._id === loca.state._id );
+    useEffect( () =>{
+        let fetchData = async () => {
+            let result = await axios.get(process.env.REACT_APP_BACKEND_URL+"house/houseById/"+loca.state._id);
+          // console.log(housesData);
+          setSearchedHouse(result.data[0])
+        }
+        fetchData();
+      },[]);
+
+      
+
+    console.log(SearchedHouse);
+
+
+    if(!props.housesData) {
+        return "...loading data";
+    }
+
+
+
+
+    return (
+        <>
+        <h1> Searched House</h1>
+        <House houseInfo = {searchedHouse}/>
+        </>
+       
+     );
+}
+ 
+export default SearchedHouse
